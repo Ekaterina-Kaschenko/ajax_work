5887b24df3e402c6efd12cb9745affe58c6c535a
a.kravchenko <take_a_fun@mail.ru>
2015-10-01 20:11:50 +0300

    js

--- script.js
+++ script.js
@@ -28,47 +28,27 @@
       console.log(data);
     }
   });
-  //   $.ajax({
-  //   dataType: 'json',
-  //   url: 'json/script2.json',
-  //   success: function(data) {
-  //     $(document.body).append(data)},
-  //   error: function(data) {
-  //     console.log(data)
-  //   }
-  // });
+  
+    var firstImg = document.createElement('img');
+    document.body.appendChild(firstImg);
+    firstImg.style.width = "200px";
+    firstImg.style.height = "auto";
+    firstImg.style.float = "left";
+    firstImg.style.marginRight = "20px";
+    firstImg.setAttribute('src', 'img/js1.jpg');
 
-  //   $.ajax({
-  //   dataType: 'json',
-  //   url: 'json/script3.json',
-  //   success: function(data) {
-  //     $(document.body).append(data)
-  //   },
-  //   error: function(data) {
-  //     console.log(data);
-  //   }
-  // });
+    var secondImg = document.createElement('img');
+    document.body.appendChild(secondImg);
+    secondImg.style.width = "200px";
+    secondImg.style.height = "auto";
+    secondImg.style.float = "left";
+    secondImg.style.marginRight = "20px";
+    secondImg.setAttribute('src', 'img/js2.png');
 
-  //   var firstImg = document.createElement('img');
-  //   document.body.appendChild(firstImg);
-  //   firstImg.style.width = "200px";
-  //   firstImg.style.height = "auto";
-  //   firstImg.style.float = "left";
-  //   firstImg.style.marginRight = "20px";
-  //   firstImg.setAttribute('src', 'img/js1.jpg');
-
-  //   var secondImg = document.createElement('img');
-  //   document.body.appendChild(secondImg);
-  //   secondImg.style.width = "200px";
-  //   secondImg.style.height = "auto";
-  //   secondImg.style.float = "left";
-  //   secondImg.style.marginRight = "20px";
-  //   secondImg.setAttribute('src', 'img/js2.png');
-
-  //   var thirdImg = document.createElement('img');
-  //   document.body.appendChild(thirdImg);
-  //   thirdImg.style.width = "200px";
-  //   thirdImg.style.height = "auto";
-  //   thirdImg.style.float = "left";
-  //   thirdImg.style.marginRight = "20px";
-  //   thirdImg.setAttribute('src', 'img/js3.jpg');
+    var thirdImg = document.createElement('img');
+    document.body.appendChild(thirdImg);
+    thirdImg.style.width = "200px";
+    thirdImg.style.height = "auto";
+    thirdImg.style.float = "left";
+    thirdImg.style.marginRight = "20px";
+    thirdImg.setAttribute('src', 'img/js3.jpg');
